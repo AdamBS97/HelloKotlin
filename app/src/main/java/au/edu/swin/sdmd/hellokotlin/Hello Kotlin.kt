2c15bc830a498847b3a9bc3626e8f2999bcd0817
AdamBS97 <69134444+AdamBS97@users.noreply.github.com>
2020-08-12 21:41:42 +1000

    Second commit. Followed along with Kotlin Bootcamp and wrote some code to learn about values and how to use them.

--- app/src/main/java/au/edu/swin/sdmd/hellokotlin/Hello Kotlin.kt
+++ app/src/main/java/au/edu/swin/sdmd/hellokotlin/Hello Kotlin.kt
@@ -1,5 +1,13 @@
 package au.edu.swin.sdmd.hellokotlin
 
-fun main() {
-    println("Hello World!")
+fun main(args: Array<String>) {
+    val isUnit = println("This is an expression")
+    println(isUnit)
+
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
+
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK"}."
+            println(message)
 }
