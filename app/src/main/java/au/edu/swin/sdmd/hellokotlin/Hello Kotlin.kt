237594c21fcb859c87c5a864c76176cce1d2b07f
AdamBS97 <69134444+AdamBS97@users.noreply.github.com>
2020-08-12 22:49:29 +1000

    Third commit. Created compact function to determine whether water needs to be changed. Continue following along w/ bootcamp.

--- app/src/main/java/au/edu/swin/sdmd/hellokotlin/Hello Kotlin.kt
+++ app/src/main/java/au/edu/swin/sdmd/hellokotlin/Hello Kotlin.kt
@@ -1,13 +1,51 @@
 package au.edu.swin.sdmd.hellokotlin
 
-fun main(args: Array<String>) {
-    val isUnit = println("This is an expression")
-    println(isUnit)
+import java.util.* // required import
 
-    val temperature = 10
-    val isHot = if (temperature > 50) true else false
-    println(isHot)
-
-    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK"}."
-            println(message)
+fun randomDay() : String {
+    val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
 }
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
+}
+
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
+}
+
+fun isTooHot(temperature: Int) = temperature > 30
+fun isDirty(dirty: Int) = dirty > 30
+fun isSunday(day: String) = day == "Sunday"
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+
+fun main() {
+    feedTheFish()
+    swim() // uses default speed
+    swim("slow") // positional argument
+    swim(speed = "turtle-like") // name parameter
+}
+
